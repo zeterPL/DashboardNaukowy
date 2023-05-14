bc7cf7e6d0c10851d6450ac9242990179f8be15a
rzymski <83671265+rzymski@users.noreply.github.com>
2023-05-14 21:51:24 +0200

    Zrobienie migracji

--- Djangosite/mainApp/admin.py
+++ Djangosite/mainApp/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from .models import University, SubjectArea
 
 # Register your models here.
 
 #tutaj będą modele które admin może edytować
-
 # Username: Admin
 # email: admin@mail.com
 # Pass: 123
 
+#drugi admin dla leniwych
+# Username: admin
+# Pass: admin
+
+admin.site.register(University)
+admin.site.register(SubjectArea)
